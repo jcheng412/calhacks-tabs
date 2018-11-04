18a2deb2910363c9546c4d395e190ecdef8c5901
halpan2001 <halpan@usc.edu>
2018-11-03 20:48:49 -0700

    login validation added to popup.html

--- background_script.js
+++ background_script.js
@@ -1,16 +1,19 @@
 
-// Initializing the extension
+// this is defining a function
 let processResults = function (results) {
 	console.log(results)
 }
 
+//this gets the full list of bookmarks
 chrome.bookmarks.getTree(function(results) {
+	//error check
 	if (chrome.runtime.lastError)
 		throw chrome.runtime.lastError
 
-	chrome.bookmarks.create({ title: "Ext Bookmarks"}, 
+//creating a folder
+	chrome.bookmarks.create({ title: "Ext Bookmarks"},
 		function (newFolder) {
-			if (chrome.runtime.lastError)
+			if (chrome.runtime.lastError)./Library/Application Support/Google/Chrome/Default/Sync Extension Settings/lcaelgondnfehcambmdhhfokjknhfahc
 				throw chrome.runtime.lastError.message
 			console.log(`${newFolder} created`)
 			createBookmark(newFolder.id)
@@ -18,8 +21,10 @@ chrome.bookmarks.getTree(function(results) {
 });
 
 
-/*
+
 console.log('Bookmark Bar')
+
+/*
 console.log(bookmarkBar)
 
 var bookmarkFolder = chrome.bookmarks.create({'parentId': bookmarkBar[0].id,
@@ -29,9 +34,10 @@ var bookmarkFolder = chrome.bookmarks.create({'parentId': bookmarkBar[0].id,
                             createBookmark(newFolder.id);
                         });
 */
+
+//this creates a bookmarks
 function createBookmark(extensionsFolderId) {
     chrome.bookmarks.create({'parentId': extensionsFolderId,
                              'title': 'Extensions doc',
                              'url': 'http://code.google.com/chrome/extensions'});
 }
-
